3576b969db45feeb8df916efc50be3d0f72dfa27
smore9982 <sameermore412@gmail.com>
2014-03-01 19:25:22 -0500

    Initial commit.

---
+++ CanIMakeWebService/src/com/canimakeit/servlets/GetStops.java
@@ -0,0 +1,37 @@
+package com.canimakeit.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+
+public class GetStops extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetStops() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		//out.print(responsePayload.toString());
+		out.flush();
+	}
+
+}
