04f8b3bcc23ffb775f6a7dd787655e1ca7d8a863
smore9982 <sameermore412@gmail.com>
2014-03-29 15:19:07 -0400

    Added code to get stop by stop id.

--- CanIMakeWebService/src/com/canimakeit/dao/StopsDao.java
+++ CanIMakeWebService/src/com/canimakeit/dao/StopsDao.java
@@ -54,6 +54,55 @@ public class StopsDao extends ParentDao {
 		}
 	}
 	
+	public StopModel getStopWithID(String stopId) throws Exception{
+		Connection connection =  getConnection();
+		StopModel model =null;
+		if(connection!=null){
+			PreparedStatement getStmt = null;
+			ResultSet resultSet = null;
+			try{
+				System.out.println("GOT A CONNECTION");
+				String getStops = "select * from stops where stop_id = ?";			
+				getStmt = connection.prepareStatement(getStops);
+				getStmt.setString(1, stopId);
+				resultSet = getStmt.executeQuery();
+				model = parseSingleStop(resultSet);			
+				resultSet.close();
+				connection.close();
+			}catch(Exception e){
+				try{
+					if(getStmt !=null){
+						getStmt.close();
+					}
+				}catch(Exception e1){};
+				
+				try{
+					if (resultSet !=null ){
+						resultSet.close();
+					}
+				}catch(Exception e2){};
+			}finally{
+				connection.close();
+			}
+			return model;			
+		
+		}else{
+			return model;
+		}
+	}
+	
+	private StopModel parseSingleStop(ResultSet rs) throws  Exception{
+		while(rs.next()){
+			String id = rs.getString(1);
+			String name = rs.getString(2);
+			String lat = rs.getString(3);
+			String lon = rs.getString(4);
+			StopModel model = new StopModel(id,name,lat,lon);
+			return model;
+		}
+		return null;
+	}
+	
 	private List<StopModel>  parseResultSet(ResultSet rs) throws Exception{
 		List<StopModel> models = new ArrayList<StopModel>();
 		while(rs.next()){
