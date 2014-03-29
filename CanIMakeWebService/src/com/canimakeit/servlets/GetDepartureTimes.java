0cb83a7cdc48323a86959bc4c400b4a751c52268
smore9982 <sameermore412@gmail.com>
2014-03-29 15:19:31 -0400

    Determining direction of trip.

--- CanIMakeWebService/src/com/canimakeit/servlets/GetDepartureTimes.java
+++ CanIMakeWebService/src/com/canimakeit/servlets/GetDepartureTimes.java
@@ -49,11 +49,15 @@ public class GetDepartureTimes extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		DepartureTimesDao timesDao = new DepartureTimesDao(dataSource);
+		StopsDao stopDao = new StopsDao(dataSource);
 		String fromId = request.getParameter("fromStationID");
 		String toId = request.getParameter("toStationID");
-		String direction = "0";
-		System.out.println("Params = " + fromId + toId + direction);
 		try{
+			StopModel fromStop = stopDao.getStopWithID(fromId);
+			StopModel toStop = stopDao.getStopWithID(toId);
+			
+			String direction = findBearing(fromStop,toStop);
+					
 			List<DepartureTimesModel> models = timesDao.getTimes(fromId, toId, direction);
 			JSONArray jsonArray = new JSONArray();
 			for(int i=0;i<models.size();i++){
@@ -71,8 +75,25 @@ public class GetDepartureTimes extends HttpServlet {
 			out.print(jsonArray.toString());
 			out.flush();
 		}catch(Exception e){
-			System.out.println("An error occured trying to get stops" + e.getMessage());
+			System.out.println("An error occured trying to get trip times" + e.getMessage());
+			e.printStackTrace();
 		}		
 	}
 
+	private String findBearing(StopModel fromStop, StopModel toStop) {
+		  double longitude1 = Double.parseDouble(fromStop.getStop_lon());
+		  double longitude2 = Double.parseDouble(toStop.getStop_lon());
+		  double latitude1 = Math.toRadians(Double.parseDouble(fromStop.getStop_lat()));
+		  double latitude2 = Math.toRadians(Double.parseDouble(toStop.getStop_lat()));
+		  double longDiff= Math.toRadians(longitude2-longitude1);
+		  double y= Math.sin(longDiff)*Math.cos(latitude2);
+		  double x=Math.cos(latitude1)*Math.sin(latitude2)-Math.sin(latitude1)*Math.cos(latitude2)*Math.cos(longDiff);
+		  double degrees = (Math.toDegrees(Math.atan2(y, x))+360)%360;
+		  
+		  if(degrees > 0 && degrees < 180){
+			  return "0";
+		  }else{
+			  return "1";
+		  }
+	}
 }
